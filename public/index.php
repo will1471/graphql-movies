1d8fa3285754ae7365c9414acd8b038b3613736e
William Redman <william.redman1989@gmail.com>
2019-03-02 12:26:17 +0000

    replace custom deferred loading with overblog/dataloader-php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-use GraphQL\Deferred;
 use GraphQL\Error\Debug;
 use GraphQL\Type\Definition\ResolveInfo;
 use Slim\Http\Request;
@@ -9,102 +8,16 @@ use Psr\Http\Message\ResponseInterface;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-class AuthorBuffer
+function group_by_key(array $data, string $key): array
 {
-    private static $ids = [];
-    private static $fields = [];
-    private static $data;
-
-    public static function add($filmId, array $fields)
-    {
-        self::$ids[] = $filmId;
-        foreach ($fields as $field) {
-            self::$fields[] = $field;
+    $a = [];
+    foreach ($data as $datum) {
+        if (!isset($a[$datum[$key]])) {
+            $a[$datum[$key]] = [];
         }
-        self::$fields = array_unique(self::$fields);
+        $a[$datum[$key]][] = $datum;
     }
-
-    public static function load(\PDO $pdo)
-    {
-        if (self::$data !== null) return;
-        $ids = array_unique(self::$ids);
-        $ids = array_map('intval', $ids);
-        $ids = join(',', $ids);
-
-        $columns = build_columns([
-            'id' => '`a`.`actor_id`',
-            'firstName' => '`a`.`first_name`',
-            'lastName' => '`a`.`last_name`',
-        ], self::$fields, 'id');
-
-        $sql = <<<SQL
-SELECT fa.film_id, $columns
-FROM actor a
-JOIN film_actor fa ON a.actor_id = fa.actor_id
-WHERE fa.film_id IN ($ids);
-SQL;
-        $select = $pdo->prepare($sql);
-        $select->execute([]);
-        $rows = $select->fetchAll(\PDO::FETCH_ASSOC);
-        self::$data = mysql_rows_to_data($rows, [
-            'actor_id' => 'id',
-            'first_name' => 'firstName',
-            'last_name' => 'lastName',
-            'film_id' => 'film_id'
-        ]);
-    }
-
-    public static function get($filmId)
-    {
-        $actors = [];
-        foreach (self::$data as $actor) {
-            if ($actor['film_id'] == $filmId) {
-                $actors[] = $actor;
-            }
-        }
-        return $actors;
-    }
-}
-
-class CategoryBuffer
-{
-    private static $ids = [];
-    private static $data;
-
-    public static function add($filmId)
-    {
-        self::$ids[] = $filmId;
-    }
-
-    public static function load(\PDO $pdo)
-    {
-        if (self::$data !== null) return;
-        $ids = array_unique(self::$ids);
-        $ids = array_map('intval', $ids);
-        $ids = join(',', $ids);
-
-        $sql = <<<SQL
-SELECT fc.film_id, name
-FROM category c
-JOIN film_category fc ON c.category_id = fc.category_id
-WHERE fc.film_id IN ($ids);
-SQL;
-        $select = $pdo->prepare($sql);
-        $select->execute([]);
-        $rows = $select->fetchAll(\PDO::FETCH_ASSOC);
-
-        foreach ($rows as $row) {
-            if (!isset(self::$data[$row['film_id']])) {
-                self::$data[$row['film_id']] = [];
-            }
-            self::$data[$row['film_id']][] = $row['name'];
-        }
-    }
-
-    public static function get($filmId)
-    {
-        return isset(self::$data[$filmId]) ? self::$data[$filmId] : [];
-    }
+    return $a;
 }
 
 function mysql_rows_to_data(array $rows, array $map): array
@@ -129,6 +42,134 @@ function build_columns(array $map, array $usedFields, string $idKey = 'id'): str
     return $columms;
 }
 
+class BatchGetActorsForMovie
+{
+    private $pdo;
+    private $promiseAdapter;
+
+    public function __construct(\PDO $pdo, \Overblog\PromiseAdapter\PromiseAdapterInterface $promiseAdapter)
+    {
+        $this->pdo = $pdo;
+        $this->promiseAdapter = $promiseAdapter;
+    }
+
+    /**
+     * @param array $keys Movie ids
+     *
+     * @return \GraphQL\Executor\Promise\Promise|mixed
+     */
+    public function __invoke(array $keys)
+    {
+        $ids = array_unique($keys);
+        $ids = array_map('intval', $ids);
+        $ids = join(',', $ids);
+
+        /* @todo only load requested rows */
+        $columns = build_columns([
+            'id' => '`a`.`actor_id`',
+            'firstName' => '`a`.`first_name`',
+            'lastName' => '`a`.`last_name`',
+        ], ['id', 'firstName', 'lastName'], 'id');
+
+        $sql = <<<SQL
+SELECT fa.film_id, $columns
+FROM actor a
+JOIN film_actor fa ON a.actor_id = fa.actor_id
+WHERE fa.film_id IN ($ids);
+SQL;
+        $select = $this->pdo->prepare($sql);
+        $select->execute([]);
+        $rows = $select->fetchAll(\PDO::FETCH_ASSOC);
+        $data = mysql_rows_to_data($rows, [
+            'actor_id' => 'id',
+            'first_name' => 'firstName',
+            'last_name' => 'lastName',
+            'film_id' => 'film_id'
+        ]);
+
+        $groups = group_by_key($data, 'film_id');
+        $result = [];
+        foreach ($keys as $key) {
+            if (isset($groups[$key])) {
+                $result[] = $groups[$key];
+            } else {
+                $result[] = [];
+            }
+        }
+        return $this->promiseAdapter->createAll($result);
+    }
+}
+
+class BatchGetMovieCategories
+{
+    private $pdo;
+    private $promiseAdapter;
+
+    public function __construct(\PDO $pdo, \Overblog\PromiseAdapter\PromiseAdapterInterface $promiseAdapter)
+    {
+        $this->pdo = $pdo;
+        $this->promiseAdapter = $promiseAdapter;
+    }
+
+    /**
+     * @param array $keys movie ids
+     * @return \GraphQL\Executor\Promise\Promise
+     */
+    public function __invoke(array $keys)
+    {
+        $ids = array_unique($keys);
+        $ids = array_map('intval', $ids);
+        $ids = join(',', $ids);
+
+        $sql = <<<SQL
+SELECT fc.film_id, name
+FROM category c
+JOIN film_category fc ON c.category_id = fc.category_id
+WHERE fc.film_id IN ($ids);
+SQL;
+        $select = $this->pdo->prepare($sql);
+        $select->execute([]);
+        $rows = $select->fetchAll(\PDO::FETCH_ASSOC);
+
+        $grouped = group_by_key($rows, 'film_id');
+        $result = [];
+        foreach ($keys as $key) {
+            if (isset($grouped[$key])) {
+                $result[] = array_map(function (array $row): string {
+                    return $row['name'];
+                }, $grouped[$key]);
+            } else {
+                $result[] = [];
+            }
+        }
+        return $this->promiseAdapter->createFulfilled($result);
+    }
+}
+
+$host = getenv('DB_HOST');
+$pdo = new PDO(
+    "mysql:host=$host;dbname=sakila;charset=utf8mb4",
+    getenv('DB_USER'),
+    getenv('DB_PASSWORD'),
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]
+);
+
+$dataLoaderPromiseAdapter = new \Overblog\PromiseAdapter\Adapter\WebonyxGraphQLSyncPromiseAdapter();
+
+$actorLoader = new \Overblog\DataLoader\DataLoader(
+    new BatchGetActorsForMovie($pdo, $dataLoaderPromiseAdapter),
+    $dataLoaderPromiseAdapter
+);
+
+$categoryLoader = new \Overblog\DataLoader\DataLoader(
+    new BatchGetMovieCategories($pdo, $dataLoaderPromiseAdapter),
+    $dataLoaderPromiseAdapter
+);
+
 $app = new \Slim\App();
 
 $app->options('/{routes:.+}', function ($request, $response, $args) {
@@ -149,22 +190,18 @@ $app->add(function (Request $request, Response $response, callable $next) {
         return $next($request, $response);
     } catch (\Exception $e) {
         echo '<pre>';
-        print_r($e);
+        print_r($e->getMessage());
+        echo "\n";
+        echo $e->getTraceAsString();
+        echo "\n\n\n";
+        if ($e->getPrevious()) {
+            print_r($e->getPrevious()->getMessage());
+            echo $e->getPrevious()->getTraceAsString();
+        }
     }
 });
 
-$app->map(['GET', 'POST'], '/', function (Request $request, Response $response): ResponseInterface {
-    $host = getenv('DB_HOST');
-    $pdo = new PDO(
-        "mysql:host=$host;dbname=sakila;charset=utf8mb4",
-        getenv('DB_USER'),
-        getenv('DB_PASSWORD'),
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false,
-        ]
-    );
+$app->map(['GET', 'POST'], '/graphql', function (Request $request, Response $response) use ($pdo, $actorLoader, $categoryLoader, $dataLoaderPromiseAdapter): ResponseInterface {
 
     $contents = file_get_contents(__DIR__ . '/schema.graphqls');
     $schema = \GraphQL\Utils\BuildSchema::build($contents);
@@ -176,7 +213,8 @@ $app->map(['GET', 'POST'], '/', function (Request $request, Response $response):
         | Debug::INCLUDE_DEBUG_MESSAGE
         | Debug::RETHROW_INTERNAL_EXCEPTIONS
     );
-    $serverConfig->setFieldResolver(function ($ctx, $args, $context, ResolveInfo $resolveInfo) use ($pdo) {
+    $serverConfig->setPromiseAdapter(new \Overblog\DataLoader\Promise\Adapter\Webonyx\GraphQL\SyncPromiseAdapter());
+    $serverConfig->setFieldResolver(function ($ctx, $args, $context, ResolveInfo $resolveInfo) use ($pdo, $actorLoader, $categoryLoader) {
 
         if (isset($ctx[$resolveInfo->fieldName])) {
             return $ctx[$resolveInfo->fieldName];
@@ -197,19 +235,11 @@ $app->map(['GET', 'POST'], '/', function (Request $request, Response $response):
         }
 
         if ($resolveInfo->fieldName == 'category') {
-            CategoryBuffer::add($ctx['id']);
-            return new Deferred(function () use ($ctx, $pdo) {
-                CategoryBuffer::load($pdo);
-                return CategoryBuffer::get($ctx['id']);
-            });
+            return $categoryLoader->load($ctx['id']);
         }
 
         if ($resolveInfo->fieldName == 'actors') {
-            AuthorBuffer::add($ctx['id'], array_keys($resolveInfo->getFieldSelection()));
-            return new Deferred(function () use ($ctx, $pdo) {
-                AuthorBuffer::load($pdo);
-                return AuthorBuffer::get($ctx['id']);
-            });
+            return $actorLoader->load($ctx['id']);
         }
 
         throw new \Exception('unsupported');
